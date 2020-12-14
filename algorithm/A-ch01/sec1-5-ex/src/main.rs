9112f185e070fbb81f76680f17147220a3b30247
fh <fhxbzz@yeah.net>
2020-12-14 14:45:16 +0800

    update

--- algorithm/A-ch01/sec1-5-ex/src/main.rs
+++ algorithm/A-ch01/sec1-5-ex/src/main.rs
@@ -21,6 +21,9 @@ fn main() -> Result<(),Box<dyn Error>> {
         "1-2" => ex_2(&mut stdin),
         "1-3" => ex_3(&mut stdin),
         "1-4" => ex_4(&mut stdin),
+        "1-5" => ex_5(&mut stdin),
+        "1-6" => ex_6(&mut stdin),
+        "1-7" => ex_7(&mut stdin),
         _ => Ok(())
     }?;
     Ok(())
@@ -52,5 +55,25 @@ fn ex_4(stdin:&mut SplitWhitespace) -> Result<(),Box<dyn Error>>{
     use std::f64::consts::PI;
     println!("{}",(n as f64 / (180.0/PI)).sin());
     println!("{}",(n as f64 / (180.0/PI)).cos());
+    Ok(())
+}
+
+fn ex_5(stdin:&mut SplitWhitespace) -> Result<(),Box<dyn Error>>{
+    let n:f64 = read!(stdin);
+    println!("{}",if 95.0*n<300.0 {95.0*n} else {95.0*n*0.85});
+    Ok(())
+}
+
+fn ex_6(stdin:&mut SplitWhitespace) -> Result<(),Box<dyn Error>>{
+    let a:i32 = read!(stdin);
+    let b:i32 = read!(stdin);
+    let c:i32 = read!(stdin);
+    println!("{}",if a+b>c && (a-b).abs()<c {"Yes"} else {"No"});
+    Ok(())
+}
+
+fn ex_7(stdin:&mut SplitWhitespace) -> Result<(),Box<dyn Error>>{
+    let n:i32 = read!(stdin);
+    println!("{}",if n%400==0 || n%100!=0 && n%4==0 {"Yes"} else {"No"});
     Ok(())
 }
